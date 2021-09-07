e96d26fe1cd0fdef1b4cbb4ea0d4e2b0e9dafe2e
Atilla Ozgur <ati.ozgur@gmail.com>
2021-09-07 15:48:21 +0200

    added examples

---
+++ 2021/examples-in-class-2021-09-07/example_import_csv.R
@@ -0,0 +1,4 @@
+library("tidyverse")
+library(readr)
+Book1 <- read_csv("Book1.csv")
+View(Book1)
---
+++ 2021/examples-in-class-2021-09-07/example_import_excel.R
@@ -0,0 +1,6 @@
+a = 10
+library(readxl)
+Book1 <- read_excel("Book1.xlsx",col_names = c('COL_A','COL_B','COL_C'))
+
+
+
